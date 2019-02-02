65adfbb2696ecabfcd816bf189f255795b9d5efd
Jak Ashuraliev <ashuraliev@gmail.com>
2019-02-01 23:19:07 -0800

    gitignore file and Go to Top Button added

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
+    <!-- Go to Top button -->
+    <a class="go-to-top" href="#"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>
     <!--START Footer-->
     <footer class="clear-fix">
         <small>&copy; 2018 - <?=date('Y')?> www.ashuraliev.com <span class="line"> | </span><span class="line-break">All Rights Reserved</span><span class="line"> | </span> Hand Coded by Jak Ashuraliev <span class="line-break"></span><span class="line"> | </span><a href="contactme.php"> Contact</a> | <a href="http://validator.w3.org/check?uri=referer" target="_blank">Valid HTML</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a>
